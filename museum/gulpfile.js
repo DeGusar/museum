98e2e4a964087002296cf41f6f41c317f029901d
Denis Gusar <degusar.wot@gmail.com>
2021-09-23 23:25:47 +0300

    feat: create file structure of the task

---
+++ museum/gulpfile.js
@@ -0,0 +1,67 @@
+const { src, dest, watch, parallel, series } = require('gulp');
+
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const uglify = require('gulp-uglify-es').default;
+const del = require('del');
+
+function browsersync() {
+    browserSync.init({
+        server: {
+          baseDir: './'  
+        }
+    });
+}
+
+function cleanDist() {
+    return del('dist')
+}
+
+
+
+function scripts() {
+    return src([
+        
+        'app/js/main.js'
+    ])
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(dest('app/js'))
+        .pipe(browserSync.stream())
+}
+
+function styles() {
+    return src('app/scss/style.scss')
+        .pipe(scss({ outputStyle: 'compressed' }))
+        .pipe(concat('style.min.css'))
+        .pipe(dest('app/css'))
+        .pipe(browserSync.stream())
+}
+
+function build() {
+    return src([
+        'app/css/style.min.css',
+        'app/fonts/**/*',
+        'app/js/main.min.js'
+        
+    ], {base:'app'})
+        .pipe(dest('dist'))
+}
+
+function watching() {
+    watch(['app/scss/**/*.scss'], styles)
+    watch(['app/js/**/*.js','!app/js/main.min.js'], scripts)
+    watch(['index.html']).on('change', browserSync.reload)
+}
+
+exports.styles = styles;
+exports.watching = watching;
+exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.cleanDist = cleanDist;
+
+
+
+exports.build = series(cleanDist, build);
+exports.default = parallel(styles, scripts, browsersync, watching);
